71a0dbb869db157be29b50f840085e41db5796fa
Sandrine2026 <sandraninovige@gmail.com>
2026-09-23 19:03:25 +0200

    correction de la function sur la page utils

--- utils.php
+++ utils.php
@@ -14,20 +14,4 @@ function moyenneTableau($notes)
     return $somme / $nombreNotes;
 }
 
-// Exercice 1.2.7 — Bonus : fonction récursive
-
-function factorielle($n)
-{
-    // Cas de base
-    if ($n == 0) {
-        return 1;
-    }
-
-    // Appel récursif
-    return $n * factorielle($n - 1);
-}
-
-echo "5! = " . factorielle(5);
-
-
 ?>
